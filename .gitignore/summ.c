a4b914ba9587288dc190b1cee9a6a54fe1bbabdd
sonamv18 <49298596+sonamv18@users.noreply.github.com>
2019-04-06 15:07:29 +0530

    Update summ.c

--- .gitignore/summ.c
+++ .gitignore/summ.c
@@ -1,1 +1,28 @@
 
+#include<stdio.h>
+void input(int num[100], int i ,int n)
+{
+    printf("enter the size of array");
+    scanf("%d",&n);
+    printf("enter the elements of array");
+    for(i=0; i<=n;i++)
+    scanf("%d",&num[i]);
+}
+int sum( *num[int i], i)
+{
+    int summ==0;
+    for(int i=0; i<=n;i++)
+    summ=summ + *num[i];
+    return summ;
+}
+void output()
+{
+    printf("the sum of given numbers is %d",summ);
+}
+int main()
+{   
+    int num[100],int i,int n,int summ;
+    input(int num[100], int i ,int n);
+    summ=sum(&num[100], i);
+    output(summ);
+}
