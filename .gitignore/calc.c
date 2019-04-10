c701d182c45b296732d20b9550f17271638fc365
sonamv18 <49298596+sonamv18@users.noreply.github.com>
2019-04-10 09:24:44 +0530

    Create calc.c

---
+++ .gitignore/calc.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+add(int a,int b)
+{
+    int res = a+b;
+    return res;
+}
+
+sub(int a, int b)
+{
+    int res=a-b;
+    return res;
+}
+
+prod(int a, int b)
+{
+    int res= a*b;
+    return res;
+}
+void main()
+{
+    int a, b, res , ch ;
+    printf("enter the first number\n");
+    scanf("%d",a);
+    printf("enter the second number\n");
+    scanf("%d", b);
+    printf("enter your choice:'+' '-' '*' ")
+    scanf(%ch", ch);
+    switch (ch)
+    {
+       case;'=' ; add( int a , int b )
+       
